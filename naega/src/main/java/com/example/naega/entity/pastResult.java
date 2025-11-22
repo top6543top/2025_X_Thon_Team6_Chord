23378ed7e6731607eac5d158ce21194509b2bd23
top6543top <top6543top@gamil.com>
2025-11-22 16:05:53 +0900

    [Feature] 엔티티 설정

--- naega/src/main/java/com/example/naega/entity/pastResult.java
+++ naega/src/main/java/com/example/naega/entity/pastResult.java
@@ -1,27 +1,27 @@
 package com.example.naega.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@Table(name = "question")
 @NoArgsConstructor
-public class Question {
+@Entity
+@Table(name = "pastResult")
+public class pastResult {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(name = "contents")
-    private String contents;
-
-    @Column(name = "response_type")
-    private String responseType;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Users users;
 }
