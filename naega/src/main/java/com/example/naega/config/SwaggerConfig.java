20fbe9d80aaa808a014459ef316e7ac78ef87d7e
eundeang <111877048+eundeang@users.noreply.github.com>
2025-11-22 18:29:47 +0900

    [CONFIG] 스웨거 설정 추가

--- naega/src/main/java/com/example/naega/config/SwaggerConfig.java
+++ naega/src/main/java/com/example/naega/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.example.naega.config;
 
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +18,20 @@ public class SwaggerConfig {
                         .title("내 프로젝트 API 문서")
                         .description("API 명세서입니다.")
                         .version("1.0.0"));
+    }
+
+    @Bean
+    public OpenApiCustomizer globalHeaderAdder() {
+        return openApi -> openApi.getPaths().values().forEach(pathItem ->
+                pathItem.readOperations().forEach(operation ->
+                        operation.addParametersItem(
+                                new Parameter()
+                                        .in("header")
+                                        .schema(new StringSchema())
+                                        .name("X-USER-ID")
+                                        .description("유저 ID 헤더")
+                        )
+                )
+        );
     }
 }
